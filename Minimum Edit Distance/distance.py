90a3db935fd269cbd78b276aea6134e90f2e860d
Ray <rayluo00@gmail.com>
2017-01-18 22:41:59 -0800

    Remodelled backtracing

--- Minimum Edit Distance/distance.py
+++ Minimum Edit Distance/distance.py
@@ -1,28 +1,66 @@
 '''
 distance.py
 
-Minimum Edit Distance Algorithm
+Minimum Edit Distance
 CSCI 404 - Natural Language Processing
-Last Modified By: Raymond Weiming Luo and Ben Ellerby
+Authors: Raymond Weiming Luo and Ben Ellerby
 
 '''
 
-def backtrace (dist, i, j, target, source):
-	mincost = dist[i][j]
+def backtrace (dist, target, source, tAlign, sAlign, opAlign, stack):
+	print "STACK: ", stack, "\n"
 
-	if (mincost == dist[i-1][j-1] and source[j-1] == target[i-1]):
-		return (i-1), (j-1), '|'
-	if (mincost == dist[i-1][j-1]+2):
-		return (i-1), (j-1), ' '
-	if (mincost == dist[i-1][j]+1):
-		return (i-1), j, 'i'
-	if (mincost == dist[i][j-1]+1):
-		return i, (j-1), 'd'
+	if not stack:
+		print tAlign, "\n", opAlign, "\n", sAlign, "\n"
+		return
+
+	coordinates = stack.pop()
+
+	if dist[coordinates[0]-1][coordinates[1]] == 0:
+		print tAlign, "\n", opAlign, "\n", sAlign, "\n"
+
+	if dist[coordinates[0]][coordinates[1]-1] == 0:
+		print tAlign, "\n", opAlign, "\n", sAlign, "\n"
+
+	if dist[coordinates[0]][coordinates[1]] == 0:
+		print tAlign, "\n", opAlign, "\n", sAlign, "\n"
+
+        node = dist[coordinates[0]][coordinates[1]]
+	
+	# Check left
+	if (coordinates[0]-1 >= 0 and (dist[coordinates[0]-1][coordinates[1]]+1) == node):
+		#tAlign = target[coordinates[0]-1] + " " + tAlign
+		#sAlign = "_ " + sAlign
+		#opAlign = "  " + opAlign
+		stack.append([coordinates[0]-1, coordinates[1]])
+		op = "i"
+
+	# Check up
+	if (coordinates[1]-1 >= 0 and (dist[coordinates[0]][coordinates[1]-1]+1) == node):
+		#tAlign = "_ " + tAlign
+		#sAlign = source[coordinates[1]-1] + " " + sAlign
+		#opAlign = "  " + opAlign
+		stack.append([coordinates[0], coordinates[1]-1])
+		op = "d"
+
+	# Check diagonal
+	if (coordinates[0]-1 >= 0 and coordinates[1]-1 >= 0 and (dist[coordinates[0]-1][coordinates[1]-1]) == node):
+		#tAlign = target[coordinates[0]-1] + " " + tAlign
+		#sAlign = source[coordinates[1]-1] + " " + sAlign
+		#opAlign = "| " + opAlign
+		stack.append([coordinates[0]-1, coordinates[1]-1])
+		op = "|"
+	
+	if (coordinates[0]-1 >= 0 and coordinates[1]-1 >= 0 and (dist[coordinates[0]-1][coordinates[1]-1]+2) == node):
+		#tAlign = target[coordinates[0]-1] + " " + tAlign
+		#sAlign = source[coordinates[1]-1] + " " + sAlign
+		#opAlign = "  " + opAlign
+		stack.append([coordinates[0]-1, coordinates[1]-1])
+		op = "s"
+
+	backtrace(dist, target, source, tAlign, sAlign, opAlign, stack, op)
 
 def distance (target, source, insertcost, deletecost, replacecost):
-	outTarget = ''
-	outSource = ''
-	outOps = ''
 	n = len(target)+1
 	m = len(source)+1
 
@@ -41,40 +79,28 @@ def distance (target, source, insertcost, deletecost, replacecost):
 
 			if (source[j-1] == target[i-1]):
 				add = 0
-			else: 
+			else:
 				add = replacecost
 
 			substcost = add + dist[i-1][j-1]
 			dist[i][j] = min(inscost, delcost, substcost)
 
 	print 'levenshtein distance =', dist[n-1][m-1], '\n'
-
-	i = n - 1
-	j = m - 1
-	while (i > 0 or j > 0):
-		i2, j2, op = backtrace(dist, i, j, target, source)
-		
-		if (op == 'i'):
-			outTarget = target[i-1] + ' ' + outTarget
-			outSource = '_ ' + outSource
-			outOps = '  ' + outOps
-		elif (op == 'd'):
-			outTarget = '_ ' + outTarget
-			outSource = source[j-1] + ' ' + outSource
-			outOps = '  ' + outOps
-		else:
-			outTarget = target[i-1] + ' ' + outTarget
-			outSource = source[j-1] + ' ' + outSource
-			outOps = op + ' ' + outOps
-		i = i2
-		j = j2
-	
-	print outTarget+'\n'+outOps+'\n'+outSource+'\n'
+	return dist
 
 if __name__=="__main__":
 	from sys import argv
+	alignments = 100
+	stack = []
+	
+	if (len(argv) > 2):
+		if (len(argv) == 4):
+			alignments = argv[3]
+                
+		dist = distance(argv[1], argv[2], 1, 1, 2)
+		print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in dist]))		
 
-	if len(argv) > 2:
-		distance(argv[1], argv[2], 1, 1, 2)
+		stack.append([len(argv[1]), len(argv[2])])
+		backtrace(dist, argv[1], argv[2], "", "", "", stack)
 	else:
 		print 'ERROR: Not enough arguments.\n'
