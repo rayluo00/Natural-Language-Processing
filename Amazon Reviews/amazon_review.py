8d6b084a1f6f30d5ae289875c8561b59b47cdbe7
Ray <rayluo00@gmail.com>
2017-03-08 15:52:24 -0800

    Work on updating dictionaries

--- Amazon Reviews/amazon_review.py
+++ Amazon Reviews/amazon_review.py
@@ -8,6 +8,7 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from sklearn.naive_bayes import MultinomialNB
+from collections import OrderedDict
 
 def ParseJSON ():
     data = []
@@ -30,19 +31,56 @@ def SplitData (data):
 
     return train, test
 
+def AddWord (dictionaries, idx, word):
+    for i in range(0, 5):
+        d = dictionaries[i]
+        if idx == i:
+            if word not in d:
+                d[word] = 1
+            else:
+                d[word] = d[word] + 1
+        else:
+            if word not in d:
+                d[word] = 0
+            else:
+                d[word] = d[word] + 1
+
+def UpdateDict (dictionaries, overall, review):
+    for word in review:
+        if overall == 1.0:
+            AddWord(dictionaries, 1, word)
+        elif overall == 2.0:
+            AddWord(dictionaries, 2, word)
+        elif overall == 3.0:
+            AddWord(dictionaries, 3, word)
+        elif overall == 4.0:
+            AddWord(dictionaries, 4, word)
+        else:
+            AddWord(dictionaries, 5, word)
+
+
 def Train (train):
     sz = len(train)
     stopWords = stopwords.words('english')
     tokenizer = RegexpTokenizer(r'\w+')
     scores = [1,2,3,4,5]
     classifer = MultinomialNB()
+    dictionaries = [{} for i in range(5)]
 
     for i in range(0, 10):
         overall = train[i]['overall']
         review = tokenizer.tokenize(train[i]['reviewText'].lower())
-        #print(review)
         review = [word for word in review if word not in stopWords]
-        #print(review,'\n')
+        UpdateDict(dictionaries,overall,review)
+
+    for j in range(0, 5):
+        d = dictionaries[j]
+        dictionaries[j] = OrderedDict(sorted(d.items(), key=lambda x: x[0]))
+
+    '''
+    for od in dictionaries:
+        print(od,'\n')
+    '''
 
 if __name__ == '__main__':
     data =  ParseJSON()
